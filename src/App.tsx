6df94b02442f228f6e4a85164444ddb6eeb807cb
Chrysanthe Belgique <belgiquc@byu.edu>
2024-03-06 10:21:13 -0700

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import jsonData from "./CollegeBasketballTeams.json";
+
+interface TeamProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+function Welcome() {
+  return (
+    <h1>
+      Welcome! This page will help you keep track of all the information about
+      all the colleges in NCAA Basketball
+    </h1>
+  );
+}
+
+class Card extends React.Component<TeamProps> {
+  render() {
+    const oneTeam = this.props;
+    return (
+      <div className="card-container">
+        <div className="card-content">
+          <h1>{oneTeam.school}</h1>
+          <h3>{oneTeam.name}</h3>
+          <h3>
+            {oneTeam.city}, {oneTeam.state}
+          </h3>
+        </div>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div>
+      {jsonData.teams.map((team) => (
+        <Card {...team} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <Welcome />
+        <TeamList />
+      </>
     </div>
   );
 }
