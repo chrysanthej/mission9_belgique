a094ac953e4beb81e22960bd50df3c8633e43ee1
Chrysanthe Belgique <belgiquc@byu.edu>
2024-03-06 10:36:08 -0700

    fixed prettierrc file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import "./App.css";
-import jsonData from "./CollegeBasketballTeams.json";
+import React from 'react';
+import './App.css';
+import jsonData from './CollegeBasketballTeams.json';
 
 interface TeamProps {
   school: string;
